97be58aa8658ed78167342587533bdc2db62b9e3
Stanislav Matsak <72652632+matsakdev@users.noreply.github.com>
2024-05-31 00:37:29 +0300

    Add driver-svc, update order-svc. Add logstash and filebeat services to docker compose

--- bin/start.sh
+++ bin/start.sh
@@ -4,11 +4,14 @@ export COMPOSE_PROJECT_NAME=widedelivery
 
 export wkdr=$PWD
 
+cd $wkdr/shared && docker compose up -d
+
 #cd $wkdr/admin-api && make start
+#cd $wkdr/admin-ui && make start
 cd $wkdr/user-svc && make start
 cd $wkdr/order-svc && make start
+cd $wkdr/driver-svc && make start
 cd $wkdr/client-api && make start
-#cd $wkdr/admin-ui && make start
 
 cd $wkdr
 make proxystart
